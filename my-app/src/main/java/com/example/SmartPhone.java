9fc06325b1ea0e72e876c1e3845b1453aebd17ff
oleh <oleh.levch3nko@gmail.com>
2026-04-27 17:21:33 +0200

    Add smartphone

---
+++ my-app/src/main/java/com/example/SmartPhone.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class SmartPhone extends Phone {
+    private String os;
+
+    public SmartPhone(String brand, String model, String os) {
+        super(brand, model);
+        this.os = os;
+    }
+}
