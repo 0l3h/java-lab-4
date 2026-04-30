0ce93cca1d9bfd98103c397930f627df026e6ebf
oleh <oleh.levch3nko@gmail.com>
2026-04-30 20:11:26 +0200

    Add sat phone class

---
+++ my-app/src/main/java/com/example/SatPhone.java
@@ -0,0 +1,15 @@
+package com.example;
+
+public class SatPhone extends KeypadPhone {
+    private String satelliteNetwork;
+
+    public SatPhone(String brand, String model, String satelliteNetwork) {
+        super(brand, model, true);
+        this.satelliteNetwork = satelliteNetwork;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Супутникова мережа: " + satelliteNetwork;
+    }
+}
